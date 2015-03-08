e2aa2d067bdb814d6874361b32129cd9962bb360
Kevin Yoon & Aaron Harris <pair+KYoon105.aamharris@devbootcamp.com>
2015-03-08 14:25:51 -0500

    able to show passingplayerlist as clickable names

--- public/templates/templates.js
+++ public/templates/templates.js
@@ -312,6 +312,18 @@ __p += '<div class="card ' +
 return __p
 };
 
+this["JST"]["templates/passingPlayer.tpl"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<li><a href="#">' +
+((__t = ( username )) == null ? '' : __t) +
+'</a></li>';
+
+}
+return __p
+};
+
 this["JST"]["templates/player.tpl"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
